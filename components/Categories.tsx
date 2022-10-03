5b99628e2da381bd70fb7c6c1ec0dac9518eed60
Rahul.B <talktorahul.b@gmail.com>
2022-10-03 18:16:08 +0530

    just commit

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useState, useEffect} from 'react'
+import { getCategories } from '../services';
+import Link from 'next/link'
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories))
+    }, []);
+    
   return (
-    <div>Categories</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+    <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+      Categories
+    </h3>
+    {categories.map((category: any) => (
+      <Link href={`/category/${category.slug}`} key={category.slug}>
+        <span className='cursor-pointer block pb-3 mb-3'>
+          {category.name}
+        </span>
+      </Link>
+    ))}
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,36 +7,76 @@ import { getPosts } from "../services"
 
 
 
-const Home: NextPage = ({posts}) => {
+// const Home: NextPage = ({posts}) => {
 
-  return (
-    <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Dash BLog</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post: { title: any; excerpt?: string }) => (
-            <PostCard post={post} key={post.title}/>
-        ))}
+//   return (
+//     <div className="container mx-auto px-10 mb-8">
+//       <Head>
+//         <title>Dash BLog</title>
+//         <link rel='icon' href='/favicon.ico' />
+//       </Head>
+//       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+//         <div className='lg:col-span-8 col-span-1'>
+//         {posts.map((post) => (
+//             <PostCard post={post} key={post.title}/>
+//         ))}
    
-        </div>
-      <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
-            <PostWidget />
-            <Categories />
+//         </div>
+//       <div className='lg:col-span-4 col-span-1'>
+//           <div className='lg:sticky relative top-8'>
+//             <PostWidget />
+//             <Categories />
+//           </div>
+//       </div>
+//     </div>
+//     </div>
+//   )
+// }
+
+// export async function getStaticProps() {
+//   const posts = (await getPosts()) || [];
+
+//   return{
+//     props: { posts }
+//   }
+// }
+
+//solve the error and rewrite the above code in typescript
+
+const Home: NextPage = ({posts} : any) => {
+      
+    return (
+      <div className="container mx-auto px-10 mb-8">
+        <Head>
+          <title>Dash BLog</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+          <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post: any) => (
+              <PostCard post={post.node} key={post.title}/>
+          ))}
+    
           </div>
+        <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'>
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
       </div>
-    </div>
-    </div>
-  )
-}
-
-export async function getStaticProps() {
-  const posts = (await getPosts()) || [];
-
-  return{
-    props: { posts }
+      </div>
+    )
   }
-}
+  
+  export async function getStaticProps() {
+    const posts = (await getPosts()) || [];
+  
+    return{
+      props: { posts }
+    }
+  }
+  
+  export default Home
+  
+ 
